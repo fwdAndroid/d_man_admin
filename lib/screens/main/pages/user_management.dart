4ef9b9c07c54ab1b201c8cd8ce4ef1475e09c4e2
fawad kaleem <fwdkaleem@gmail.com>
2023-11-24 04:15:02 +0500

    User Management

--- lib/screens/main/pages/user_management.dart
+++ lib/screens/main/pages/user_management.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_datagrid/datagrid.dart';
 
 class UserManagement extends StatefulWidget {
   const UserManagement({super.key});
@@ -8,8 +9,122 @@ class UserManagement extends StatefulWidget {
 }
 
 class _UserManagementState extends State<UserManagement> {
+  List<Employee> employees = <Employee>[];
+  late EmployeeDataSource employeeDataSource;
+
+  @override
+  void initState() {
+    super.initState();
+    employees = getEmployeeData();
+    employeeDataSource = EmployeeDataSource(employeeData: employees);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SfDataGrid(
+        source: employeeDataSource,
+        columnWidthMode: ColumnWidthMode.fill,
+        columns: <GridColumn>[
+          GridColumn(
+              columnName: 'id',
+              label: Container(
+                  padding: EdgeInsets.all(16.0),
+                  alignment: Alignment.center,
+                  child: Text(
+                    'ID',
+                  ))),
+          GridColumn(
+              columnName: 'name',
+              label: Container(
+                  padding: EdgeInsets.all(8.0),
+                  alignment: Alignment.center,
+                  child: Text('Name'))),
+          GridColumn(
+              columnName: 'phoneNumber',
+              label: Container(
+                  padding: EdgeInsets.all(8.0),
+                  alignment: Alignment.center,
+                  child: Text(
+                    'Phone Number',
+                    overflow: TextOverflow.ellipsis,
+                  ))),
+          GridColumn(
+              columnName: 'address',
+              label: Container(
+                  padding: EdgeInsets.all(8.0),
+                  alignment: Alignment.center,
+                  child: Text('Address'))),
+        ],
+      ),
+    );
+  }
+
+  List<Employee> getEmployeeData() {
+    return [
+      Employee(10001, 'James', '+92131415532', "256 B Eden Garden"),
+      Employee(10002, 'Kathryn', '+91245155235', "2536 B Eden Garden"),
+      Employee(10003, 'Lara', '+23095235', "256 B Eden Garden"),
+      Employee(10004, 'Michael', '+2452355', "256 B Eden Garden"),
+      Employee(10005, 'Martin', '+23095235', "256 B Eden Garden"),
+      Employee(10006, 'Newberry', '+23095235', "256 B Eden Garden"),
+      Employee(10007, 'Balnc', '+23095235', "256 B Eden Garden"),
+      Employee(10008, 'Perry', '+23095235', "256 B Eden Garden"),
+      Employee(10009, 'Gable', '+23095235', "256 B Eden Garden"),
+      Employee(10010, 'Grimes', '+23095235', "256 B Eden Garden")
+    ];
+  }
+}
+
+/// Custom business object class which contains properties to hold the detailed
+/// information about the employee which will be rendered in datagrid.
+class Employee {
+  /// Creates the employee class with required details.
+  Employee(this.id, this.name, this.phoneNumber, this.address);
+
+  /// Id of an employee.
+  final int id;
+
+  /// Name of an employee.
+  final String name;
+
+  /// phoneNumber of an employee.
+  final String phoneNumber;
+
+  /// address of an employee.
+  final String address;
+}
+
+/// An object to set the employee collection data source to the datagrid. This
+/// is used to map the employee data to the datagrid widget.
+class EmployeeDataSource extends DataGridSource {
+  /// Creates the employee data source class with required details.
+  EmployeeDataSource({required List<Employee> employeeData}) {
+    _employeeData = employeeData
+        .map<DataGridRow>((e) => DataGridRow(cells: [
+              DataGridCell<int>(columnName: 'id', value: e.id),
+              DataGridCell<String>(columnName: 'name', value: e.name),
+              DataGridCell<String>(
+                  columnName: 'phoneNumber', value: e.phoneNumber),
+              DataGridCell<String>(columnName: 'address', value: e.address),
+            ]))
+        .toList();
+  }
+
+  List<DataGridRow> _employeeData = [];
+
+  @override
+  List<DataGridRow> get rows => _employeeData;
+
+  @override
+  DataGridRowAdapter buildRow(DataGridRow row) {
+    return DataGridRowAdapter(
+        cells: row.getCells().map<Widget>((e) {
+      return Container(
+        alignment: Alignment.center,
+        padding: EdgeInsets.all(8.0),
+        child: Text(e.value.toString()),
+      );
+    }).toList());
   }
 }
