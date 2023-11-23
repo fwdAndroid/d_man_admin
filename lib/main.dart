ce7c12e2b1c9ea53abc0fa868d1cc522bbe95ace
fawad kaleem <fwdkaleem@gmail.com>
2023-11-24 04:36:59 +0500

    Firebase Intialized

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,13 @@ import 'package:d_man_admin/splash/splash.dart';
 import 'package:d_man_admin/utils/helper.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
